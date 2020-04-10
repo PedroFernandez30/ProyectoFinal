2529b0623275ca0780f653593d1f8134aa851c61
PedroFernandez30 <pedrofj1999@gmail.com>
2020-04-10 15:02:00 +0200

    Bootstrap funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+require('../css/app.scss');
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
